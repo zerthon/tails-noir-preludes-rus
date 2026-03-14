92f7b867575254093203bfc071cbe9de48e07936
zerth0n <ruban120398@gmail.com>
2026-03-14 16:54:41 +0200

    testing

--- tools/scripts/index.js
+++ tools/scripts/index.js
@@ -1,7 +1,7 @@
 // Код целиком написан нейросетью claude-3.5-sonnet
 // Спасибо, что работает
 
-const fileName = 'Radio_1-4';
+const fileName = 'Bunks_1-2';
 const filePath = '../../Json-TailsRUS_P/BackboneStories/Content/Data/Dialogues/Act_I'
 
 const fs = require('fs');
